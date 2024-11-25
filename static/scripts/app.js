70c6536d6aa159dfc572d2e7cf959b63cd789606
heyitssatyam <kumarsatyam2003@gmail.com>
2024-11-25 23:34:31 +0530

    added loader for scanning

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,9 +1,16 @@
 const fileInput = document.getElementById("file-input");
 const analyzeButton = document.getElementById("analyze-button");
-const report = document.getElementById("report");
+const loader = document.querySelector(".loaderRectangle");
 
 fileInput.addEventListener("change", () => {
   analyzeButton.disabled = !fileInput.files.length;
+});
+
+analyzeButton.addEventListener("click", () => {
+  if (fileInput.files.length > 0) {
+    loader.classList.remove("hidden");
+    analyzeButton.classList.add("hidden");
+  }
 });
 
 // analyzeButton.addEventListener("click", async () => {
